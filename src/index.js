0045ac531e9e0577768cdc7e0c2aad65e41696d8
Geovanni Pacheco <geovanni.m.pacheco@gmail.com>
2017-11-10 17:28:49 -0200

    Fix react error

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 
+const reactSelection = document.createElement('selection');
+document.body.appendChild(reactSelection);
+
 // Now we can render our application into it
-render( <App />, document.body );
+render( <App />, reactSelection );
