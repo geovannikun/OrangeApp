6589716a5b185947eaf19cee8fa91a5fccc139bd
Geovanni Pacheco <geovanni.m.pacheco@gmail.com>
2018-05-15 16:51:55 -0300

    Turn acceptable files into a list

--- src/stores/AppStore.ts
+++ src/stores/AppStore.ts
@@ -1,5 +1,10 @@
 import { observable, autorun, action } from 'mobx';
 
 export default class AppStore {
-  @observable public acceptableImageTypes: string = 'image/jpeg, image/png';
+  @observable public acceptableImageTypes: string[] = [
+    'image/jpeg',
+    'image/png',
+    'application/zip',
+    'application/x-wine-extension-sketch',
+  ];
 }
