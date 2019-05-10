0ac90f785f3a7546149e8cfb8b0e4c9296cd5e6f
Geovanni Martins Pacheco <geovanni.pacheco@projetoomni.com>
2019-05-10 15:30:02 -0300

    Fix artboard name

--- src/renderer/components/canvas/KonvaCanvas/elements/KCArtboard.tsx
+++ src/renderer/components/canvas/KonvaCanvas/elements/KCArtboard.tsx
@@ -1,6 +1,6 @@
 import Konva from 'konva'
 import React from 'react'
-import { Group, Text, Rect } from 'react-konva'
+import { Group, Rect, Text } from 'react-konva'
 import { IOrangeItem, OrangeArtboard } from '../../../../classes'
 import RenderUtils from '../RenderUtils'
 import Item from './KCItem'
@@ -12,12 +12,16 @@ interface ArtboardProps {
 
 class KCArtboard extends Item<ArtboardProps> {
 
+  private name: Konva.Text | null = null
   private nameGap = 20
   private nameFontSize = 16
-  private nameColor = '#FFF'
 
   public renderSubitems(items: IOrangeItem[]): Array<JSX.Element | undefined> {
     return items && items.map((item) => RenderUtils.renderItem(item, this.props.select))
+  }
+
+  public componentDidMount() {
+    this.name!.cache({})
   }
 
   public render(): JSX.Element {
@@ -30,7 +34,13 @@ class KCArtboard extends Item<ArtboardProps> {
           onClick={this.props.select(this.props.item)}
           {...{x, y}}
         >
-          <Text text={this.props.item.name} y={-this.nameGap} fontSize={this.nameFontSize} fontStyle={{}}/>
+          <Text
+            ref={(node) => this.name = node}
+            text={this.props.item.name}
+            y={-this.nameGap}
+            fontSize={this.nameFontSize}
+            filters={[Konva.Filters.Invert]}
+          />
           <Rect {...rectStyle}/>
           {this.renderSubitems(this.props.item.children)}
         </Group>
