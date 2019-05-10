54623a92ead09c24dbfe0eacfcf41d07ff30b607
Geovanni Pacheco <geovanni.m.pacheco@gmail.com>
2019-05-10 11:31:21 -0300

    Fix konva artboard

--- src/renderer/components/canvas/KonvaCanvas/elements/KCArtboard.tsx
+++ src/renderer/components/canvas/KonvaCanvas/elements/KCArtboard.tsx
@@ -1,6 +1,6 @@
 import Konva from 'konva'
 import React from 'react'
-import { Group, Text } from 'react-konva'
+import { Group, Text, Rect } from 'react-konva'
 import { IOrangeItem, OrangeArtboard } from '../../../../classes'
 import RenderUtils from '../RenderUtils'
 import Item from './KCItem'
@@ -11,19 +11,26 @@ interface ArtboardProps {
 }
 
 class KCArtboard extends Item<ArtboardProps> {
+
+  private nameGap = 20
+  private nameFontSize = 16
+
   public renderSubitems(items: IOrangeItem[]): Array<JSX.Element | undefined> {
     return items && items.map((item) => RenderUtils.renderItem(item, this.props.select))
   }
 
   public render(): JSX.Element {
     if (this.props.item) {
+      const { x, y, ...rectStyle } = this.itemToCSS(this.props.item)
+
       return(
         <Group
           key={this.props.item.id}
           onClick={this.props.select(this.props.item)}
-          {...this.itemToCSS(this.props.item)}
+          {...{x, y}}
         >
-          <Text text={this.props.item.name}/>
+          <Text text={this.props.item.name} y={-this.nameGap} fontSize={this.nameFontSize}/>
+          <Rect {...rectStyle}/>
           {this.renderSubitems(this.props.item.children)}
         </Group>
       )
